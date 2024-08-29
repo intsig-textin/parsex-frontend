c590e29d0336ecc71697daae68857911a612e998
fuller_xu <fuller_xu@intsig.net>
2024-08-29 21:15:05 +0800

    fix: default api

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -70,14 +70,14 @@ const errorHandler = async (error: ResponseError<any>): Promise<Response> => {
 };
 
 export const envUrl = {
-  [env.TEST]: 'https://textin-sandbox.intsig.com',
-  // [env.TEST]: 'https://api.textin.com',
+  // [env.TEST]: 'https://textin-sandbox.intsig.com',
+  [env.TEST]: 'https://api.textin.com',
   [env.PRODUCTION]: 'https://web-api.textin.com',
 };
 
 export const ocrUrl = {
-  [env.TEST]: 'https://textin-sandbox.intsig.com',
-  // [env.TEST]: 'https://api.textin.com',
+  // [env.TEST]: 'https://textin-sandbox.intsig.com',
+  [env.TEST]: 'https://api.textin.com',
   [env.PRODUCTION]: 'https://api.textin.com',
 };
 
